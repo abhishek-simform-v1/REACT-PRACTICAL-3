aff9b0a71d12989541b357722abd5c08e3842343
abhishek Vaghela <abhishek.v@simformsolutions.com>
2023-05-02 12:25:02 +0530

    date now mutable

--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -27,8 +27,11 @@ export const TodoContext = createContext<{
 
 // Defining the "Context" component
 const Context = ({ children }: Props) => {
+  let InitialTask = removeOldTask(
+    JSON.parse(localStorage.getItem("data")!) || []
+  );
   // Declaring the "data" and "setData" state variables using useState hook and initializing it with the data from local storage or an empty array
-  const [data, setData] = useState<DataItem[]>(init);
+  const [data, setData] = useState<DataItem[]>(InitialTask);
 
   // Updating the data in local storage whenever the "data" state variable changes
   useEffect(() => {
@@ -37,26 +40,37 @@ const Context = ({ children }: Props) => {
   // Function to check if the to-do items in local storage have expired
 
   // Function to initialize the data state variable
-  function checkSessionExpiry(storageKey: string) {
-    const data = JSON.parse(localStorage.getItem(storageKey) || "[]"); // Parsing the data from local storage
-    let currentDate = new Date().getDate();
-    // Checking each item in the data to see if it was created on the current date
-    data.map((items: DataItem) => {
-      if (currentDate !== items.createdAt) {
-        localStorage.removeItem(storageKey); // Removing the data from local storage if it has expired
-      }
-    });
-    return data;
+  // function checkSessionExpiry(storageKey: string) {
+  //   const data = JSON.parse(localStorage.getItem(storageKey) || "[]"); // Parsing the data from local storage
+  //   let currentDate = new Date().getDate();
+  //   // Checking each item in the data to see if it was created on the current date
+  //   data.map((items: DataItem) => {
+  //     if (currentDate !== items.createdAt) {
+  //       localStorage.removeItem(storageKey); // Removing the data from local storage if it has expired
+  //     }
+  //   });
+  //   return data;
+  // }
+  function removeOldTask(list: DataItem[]): DataItem[] {
+    let date: Date = new Date();
+
+    let filteredArray: DataItem[] = list.filter(
+      (task) => task.createdAt == date.getDate()
+    );
+
+    localStorage.setItem("data", JSON.stringify(filteredArray));
+
+    return filteredArray;
   }
 
-  function init() {
-    const localdata = checkSessionExpiry("data");
+  // function init() {
+  //   const localdata = removeOldTask(JSON.(localStorage.getItem("data")));
 
-    if (localdata !== null) {
-      return localdata; // Returning the data from local storage if it exists
-    }
-    return []; // Returning an empty array if no data exists in local storage
-  }
+  //   if (localdata !== null) {
+  //     return localdata; // Returning the data from local storage if it exists
+  //   }
+  //   return []; // Returning an empty array if no data exists in local storage
+  // }
 
   // Rendering the "TodoContext.Provider" with the "data" and "setData" state variables as its value and the "children" as its children
   return (
